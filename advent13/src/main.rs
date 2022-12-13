c7f06813e798f22206ec2ba5bb940f7efeb7f7bb
Lukas Holecek <hluk@email.cz>
2022-12-13 15:36:11 +0100

    Refactor day 13

--- advent13/src/main.rs
+++ advent13/src/main.rs
@@ -8,6 +8,22 @@ enum Node {
     Nodes(Vec<Node>),
 }
 
+macro_rules! nodes {
+    ( $( $x:expr ),* ) => {
+        Node::Nodes(vec![
+            $( $x, )*
+        ])
+    };
+}
+
+macro_rules! values {
+    ( $( $x:expr ),* ) => {
+        Node::Nodes(vec![
+            $( Node::Value($x), )*
+        ])
+    };
+}
+
 #[derive(Debug)]
 struct Operands {
     left: Node,
@@ -18,41 +34,30 @@ impl Node {
     fn from(line: &str) -> Self {
         let mut stack: Vec<Vec<Node>> = vec![];
         let mut nodes: Vec<Node> = vec![];
-
         let mut maybe_value: Option<u8> = None;
         for c in line.chars() {
             if c.is_ascii_digit() {
                 let d = c.to_digit(10).unwrap() as u8;
-                if let Some(v) = maybe_value {
-                    maybe_value = Some(v * 10 + d)
-                } else {
-                    maybe_value = Some(d);
-                }
+                maybe_value = Some(maybe_value.unwrap_or(0) * 10 + d)
             } else {
+                if let Some(v) = maybe_value {
+                    nodes.push(Node::Value(v));
+                    maybe_value = None;
+                }
+
                 match c {
                     '[' => {
                         stack.push(nodes);
                         nodes = vec![];
                     },
-                    ',' => {
-                        if let Some(v) = maybe_value {
-                            nodes.push(Node::Value(v));
-                        }
-                    },
                     ']' => {
-                        if let Some(v) = maybe_value {
-                            nodes.push(Node::Value(v));
-                        }
-
                         let mut parent = stack.pop().unwrap();
                         parent.push(Node::Nodes(nodes));
                         nodes = parent;
                     },
-                    _ => {
-                        panic!("Unexpected char: {}", c);
-                    },
+                    ',' => {},
+                    _ => panic!("Unexpected char: {}", c),
                 }
-                maybe_value = None;
             }
         }
         nodes.pop().unwrap()
@@ -67,35 +72,25 @@ impl PartialOrd for Node {
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
-        match self {
-            Self::Value(l) => {
-                match other {
-                    Self::Value(r) => l.cmp(r),
-                    Self::Nodes(_) => {
-                        Self::Nodes(vec![Self::Value(*l)]).cmp(other)
-                    },
-                }
-            },
-            Self::Nodes(l) => {
-                match other {
-                    Self::Value(v) => {
-                        self.cmp(&Self::Nodes(vec![Self::Value(*v)]))
-                    },
-                    Self::Nodes(r) => {
-                        let ord = l.iter().zip(r.iter()).find_map(|(l, r)| {
-                                let ord = l.cmp(r);
-                                if ord == Ordering::Equal {
-                                    None
-                                } else {
-                                    Some(ord)
-                                }
-                            }).unwrap_or(Ordering::Equal);
+        match (self, other) {
+            (Self::Value(l), Self::Value(r)) => l.cmp(r),
+            (Self::Value(l), Self::Nodes(_)) =>
+                Self::Nodes(vec![Self::Value(*l)]).cmp(other),
+            (Self::Nodes(_), Self::Value(v)) =>
+                self.cmp(&Self::Nodes(vec![Self::Value(*v)])),
+            (Self::Nodes(l), Self::Nodes(r)) => {
+                let ord = l.iter().zip(r.iter()).find_map(|(l, r)| {
+                        let ord = l.cmp(r);
                         if ord == Ordering::Equal {
-                            l.len().cmp(&r.len())
+                            None
                         } else {
-                            ord
+                            Some(ord)
                         }
-                    }
+                    }).unwrap_or(Ordering::Equal);
+                if ord == Ordering::Equal {
+                    l.len().cmp(&r.len())
+                } else {
+                    ord
                 }
             },
         }
@@ -121,16 +116,8 @@ fn solution1(input: &str) -> usize {
 
 fn divs() -> [Node; 2] {
     [
-        Node::Nodes(vec![
-            Node::Nodes(vec![
-                Node::Value(2)
-            ])
-        ]),
-        Node::Nodes(vec![
-            Node::Nodes(vec![
-                Node::Value(6)
-            ])
-        ]),
+        nodes![values![2]],
+        nodes![values![6]],
     ]
 }
 
@@ -169,34 +156,11 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        assert_eq!(Node::from("[1,2]"), Node::Nodes(vec![
-                Node::Value(1),
-                Node::Value(2),
-        ]));
-        assert_eq!(Node::from("[1,10,2]"), Node::Nodes(vec![
-                Node::Value(1),
-                Node::Value(10),
-                Node::Value(2),
-        ]));
-        assert_eq!(Node::from("[[1]]"), Node::Nodes(vec![
-                Node::Nodes(vec![
-                    Node::Value(1)
-                ])
-        ]));
-        assert_eq!(Node::from("[[1],[2]]"), Node::Nodes(vec![
-                Node::Nodes(vec![
-                    Node::Value(1)
-                ]),
-                Node::Nodes(vec![
-                    Node::Value(2)
-                ]),
-        ]));
-        assert_eq!(Node::from("[[1],2]"), Node::Nodes(vec![
-                Node::Nodes(vec![
-                    Node::Value(1)
-                ]),
-                Node::Value(2),
-        ]));
+        assert_eq!(Node::from("[1,2]"), values![1, 2]);
+        assert_eq!(Node::from("[1,10,2]"), values![1, 10, 2]);
+        assert_eq!(Node::from("[[1]]"), nodes![values![1]]);
+        assert_eq!(Node::from("[[1],[2]]"), nodes![values![1], values![2]]);
+        assert_eq!(Node::from("[[1],2]"), nodes![values![1], Node::Value(2)]);
     }
 
     #[test]
